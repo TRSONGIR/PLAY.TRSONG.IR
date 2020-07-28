dc11625ad1125ab444875e0c2d9db02ed876e492
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-07-28 13:16:21 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-$links = parse_ini_file('links.ini');
- 
-if(isset($_GET['l']) && array_key_exists($_GET['l'], $links)){
-    header('Location: ' . $links[$_GET['l']]);
-}
-else{
-    header('HTTP/1.0 404 Not Found');
-    echo 'Unknown link.';
-}
+$gdrive ="https://drive.google.com/uc?id=";
+$gdriveid = $_GET['id'];
+header ("Location: $gdrive.$gdriveid");
+exit;
+?>
